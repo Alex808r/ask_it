940f02cd1424d96fe75c1878dc6b867b81ed3a80
Alex808r <fromxlam@gmail.com>
2021-08-23 18:10:36 +0300

    Add action create update QuestionController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,40 @@
 class QuestionsController < ApplicationController
+
   def index
     @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    #render plain: params
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+    # @question.save ? redirect_to questions_path : render :new
+  end
+
+  def edit
+    @question = Question.find_by(id: params[:id]) # или Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find_by(id: params[:id])
+    if @question.update(question_params)
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   #root 'hello#start' # или root to: 'hello#start', as: 'home'
 
   root 'pages#index'
-  get '/questions', to: 'questions#index' #resources :questions, only: :index
 
+  # get '/questions', to: 'questions#index' #resources :questions, only: :index
+  # get '/questions/new', to: 'questions#new'
+  # post '/questions', to: 'questions#create'
+  # get 'questions/:id/edit', to: 'questions#edit'
+
+  resources :questions, only: %i[index new edit create update]
 
 end
