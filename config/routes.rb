6b61b9028c782355bc977099cc9c712f5a4feb86
Alex808r <fromxlam@gmail.com>
2021-08-23 14:56:08 +0300

    Add QuestionsController views index routes. lesson 2 Done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   #root 'hello#start' # или root to: 'hello#start', as: 'home'
 
   root 'pages#index'
+  get '/questions', to: 'questions#index' #resources :questions, only: :index
+
 
 end
