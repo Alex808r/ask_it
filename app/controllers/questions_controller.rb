0da6c27e6396ac1f9c96815c2a568790c46359b4
Alex808r <fromxlam@gmail.com>
2021-08-24 13:29:13 +0300

    Add method set question! to QuestionsController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,13 @@
 class QuestionsController < ApplicationController
+  before_action :set_question!, only: %i[show edit update destroy ] # вынесли общий код в метод и определили загрузку
+  # метода для нужных action
 
   def index
     @questions = Question.all
   end
 
   def show
-    @question = Question.find_by(id: params[:id])
+    # @question = Question.find_by(id: params[:id])
   end
 
   def new
@@ -16,7 +18,7 @@ class QuestionsController < ApplicationController
     #render plain: params
     @question = Question.new(question_params)
     if @question.save
-      flash[:success] = 'Question created!'
+      flash[:success] = "Question created!"
       redirect_to questions_path
     else
       render :new
@@ -25,13 +27,13 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    @question = Question.find_by(id: params[:id]) # или Question.find(params[:id])
+    # @question = Question.find_by(id: params[:id]) # или Question.find(params[:id])
   end
 
   def update
-    @question = Question.find_by(id: params[:id])
+    # @question = Question.find_by(id: params[:id])
     if @question.update(question_params)
-      flash[:success] = 'Question updated!'
+      flash[:success] = "Question updated!"
       redirect_to questions_path
     else
       render :edit
@@ -39,9 +41,9 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question = Question.find_by(id: params[:id])
+    # @question = Question.find_by(id: params[:id])
     @question.destroy
-    flash[:success] = 'Question destroy!'
+    flash[:success] = "Question destroy!"
     redirect_to questions_path
   end
 
@@ -49,5 +51,10 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:title, :body)
+  end
+
+  def set_question!
+    @question = Question.find(params[:id])
+    # @question = Question.find_by(id: params[:id]) аналогичная запись
   end
 end
