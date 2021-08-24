87c0c0f8c0516b1077c1aabcc156394047073472
Alex808r <fromxlam@gmail.com>
2021-08-24 14:53:29 +0300

    Add method create AnswersController

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,27 @@
 class AnswersController < ApplicationController
+  before_action :set_question!, only: %i[create]
+
+  def create
+    #render plain: params
+    #@question = Question.find(params[:question_id])
+    @answer = @question.answers.build(answer_params)
+    if @answer.save
+      flash[:success] = 'Answer created'
+      redirect_to question_path(@question)
+    else
+      render 'questions/show'
+
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+
+  def set_question!
+    @question = Question.find(params[:question_id])
+  end
 
 end
