52df107aa7ce76caa9a45c11cddeca74bb12f516
Alex808r <fromxlam@gmail.com>
2021-08-24 17:56:21 +0300

    Add helper full title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "AskIt"
+    if page_title.present?
+      "#{page_title} | #{base_title}"
+    else
+      base_title
+    end
+  end
+
+  # def full_title(page_title = '')
+  #   base_title = "Ask It"
+  #   if page_title.empty?
+  #     base_title
+  #   else
+  #     page_title + " | " + base_title
+  #   end
+  # end
+
+
 end
