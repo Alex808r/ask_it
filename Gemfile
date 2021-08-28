1808380bf065de68bdabdd5ef42e4468a4429688
Alex808r <fromxlam@gmail.com>
2021-08-28 19:30:21 +0300

    Add uprate User update password old password validate User. Lesson 9 Done.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.2'
 
+gem 'valid_email2', '~> 4.0'
+
+
 gem 'draper', '~> 4.0'
 
 #gem 'kaminari',                   '~> 1.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,35 @@
 #  index_users_on_email  (email) UNIQUE
 #
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true # проверка уникальности на уровен ror, не сработает если
-                                                              # обращаться напряму к базе данных. Дополнительно проверку уникльности
-                                                              # делаем на уровне самой базы данных, для этого прописываем это в миграции перед запуском null: false, index: {unique: true}
-                                                              #validates :name, presence: true
+  attr_accessor :old_password
 
-  has_secure_password # для bcrypt - пароль и подтверждение
+  
+  validates :email, presence: true, uniqueness: true, 'valid_email_2/email': true
+  has_secure_password validations: false # для bcrypt - пароль и подтверждение
+
+  validate :password_presence
+
+  validates :password, confirmation: true, allow_blank: true, length: {minimum: 8, maximum: 70}
+
+  validate :correct_old_password, on: :update, if: -> { password.present? }
+
+  validate :password_complexity
+  private
+
+  def password_complexity
+    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+
+    errors.add :password, 'complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
+
+  def password_presence
+    errors.add(:password, :blank) unless password_digest.present?
+  end
+
+  def correct_old_password
+    return if BCrypt::Password.new(password_digest_was).is_password?(old_password)
+
+    errors.add :old_password, 'is incorrect'
+  end
 end
