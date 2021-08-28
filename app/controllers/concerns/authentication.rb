92427d681489f776e02dab1ff12240b5e693398d
Alex808r <fromxlam@gmail.com>
2021-08-28 17:28:40 +0300

    Session Done

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -21,6 +21,19 @@ module Authentication
       @current_user = nil
     end
 
+    def require_no_authentication
+      return unless user_signed_in? # или return if !user_siged_in?
+      flash[:warning] = "You are already signed in"
+      redirect_to root_path
+    end
+
+    def require_authentication
+      return if user_signed_in?
+
+      flash[:warning] = "You are not signed in!"
+      redirect_to root_path
+    end
+
 
 
     helper_method :current_user, :user_signed_in?
